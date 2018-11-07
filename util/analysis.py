24a5f22f06578dde2de8376fea8ba236a28e89d3
Ziqiang Guan <wolfgang.guan56@gmail.com>
2018-11-07 14:02:12 -0500

    added additional comments

--- util/analysis.py
+++ util/analysis.py
@@ -9,6 +9,12 @@ class MusicAnalysis:
     self.dataset = HaydnQuartetRawDataset()
 
   def min_max_pitch(self, verbose=True):
+    '''
+    Computes the min and the max pitch step in the dataset.
+
+    Returns:
+      float, float: A tuple containing the minimum and maximum pitch step, in that order.
+    '''
     if verbose:
       start = time()
       print("Finding min/max pitches...")
@@ -20,6 +26,12 @@ class MusicAnalysis:
     return min_pitch, max_pitch
 
   def to_pitch_steps(self, verbose=True):
+    '''
+    Turns the dataset into a nested List of pitch steps. The Lists have the dimensions (# of pieces) x (# of parts) x (# of notes).
+
+    Returns:
+      List: A multi-dimensional of List containing the pitch steps.
+    '''
     return list(map(self.__score_to_pitch_steps__, self.dataset))
 
   def __score_to_pitch_steps__(self, score, verbose=True):
