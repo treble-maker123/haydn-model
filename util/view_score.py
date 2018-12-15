5f42e2356304c988569c469eb215afa49a6400e7
Ziqiang Guan <wolfgang.guan56@gmail.com>
2018-12-15 16:06:44 -0500

    commented out transposition

--- util/view_score.py
+++ util/view_score.py
@@ -1,6 +1,12 @@
+import os
 from music21 import converter
 
 if __name__ == "__main__":
-    file_path = "./outputs/4159EF_12-10_04-50-43.pgz"
+    # file_path = "./outputs/8C50D6_12-13_21-52-07.pgz"
+    cdir = os.getcwd()
+    file_path = cdir + "/data/5795802993.pgz"
     score = converter.thaw(file_path)
+
+    # score = score.transpose(3)
+    # converter.freeze(score, fp=file_path)
     score.show()
